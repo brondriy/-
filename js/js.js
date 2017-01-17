e46c772bf75820eaa5b0804755caf169624b3499
brondriy <ahremchik.mani@gmail.com>
2017-01-17 21:05:02 +0300

    step9btn

--- js/js.js
+++ js/js.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
-     $(".p1-center-text").animate({
+     $(".p1-center-text").animate(
+    {
          top: "-=20",
          opacity:1
      }, 1200);
+    $(".btn-next").animate(
+    {
+        opacity:1
+    }, 2000)
     $(".white").animate(
     {
         opacity: 0.85
-    }, 5000)
+    }, 5000);
 
 });
