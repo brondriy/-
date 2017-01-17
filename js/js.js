940ed02d551cbb65b28db0083d3ebc82f77f2bf0
brondriy <ahremchik.mani@gmail.com>
2017-01-17 11:47:54 +0300

    step8-animation

--- js/js.js
+++ js/js.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
- $(".p1-center-text").animate({
-     top: "-=20",
-     opacity:1
- }, 800);
+     $(".p1-center-text").animate({
+         top: "-=20",
+         opacity:1
+     }, 1200);
+    $(".white").animate(
+    {
+        opacity: 0.85
+    }, 5000)
 
 });
