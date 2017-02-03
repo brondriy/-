58e887497eee98e6f81765a940d8ef1d01588b98
brondriy <ahremchik.mani@gmail.com>
2017-02-04 02:03:43 +0300

    anim2

--- js/js.js
+++ js/js.js
@@ -1,5 +1,6 @@
 var active = 1;
 var active2 = 1;
+var change = false;
 
 $(document).ready(function(){
     $(".p1-center-text").animate(
@@ -14,7 +15,7 @@ $(document).ready(function(){
     $(".white").animate(
     {
         opacity: 0.9
-    }, 5000);
+    }, 5000, function(){change = false;});
 
 });
 
@@ -39,17 +40,49 @@ $("li.m2-5").click(function(){changem2item(active2,5);});
 function changepage(_from,_to)
 {
     if( _to == active ) return;
-    $(".main"+_from).fadeOut(600,function(){$(".main"+_to).slideDown(600, function(){if(_to == 2 ){$("div[class='m2-']").hide(); $("div.m2-1").slideDown(600);}});});
-    $(".m1-"+_from).removeClass("active");
-    $(".m1-"+_to).addClass("active");
-    active = _to;
+    if( !change )
+    {
+        change = true;
+        $(".main"+_from).fadeOut(600,
+        function()
+        {
+            $("div.m2-"+active2).hide();
+            $("li.m2-"+active2).removeClass("item-active");
+            $("li.m2-1").addClass("item-active");
+            $(".main"+_to).slideDown(600,
+            function()
+            {
+                if(_to == 2 )
+                {
+                    active2 = 1;
+                    $("div.m2-1").slideDown(600,
+                    function()
+                    {
+                        change = false;
+                    });
+                }
+                else
+                {
+                    change = false;
+                }
+            });
+        });
+        $(".m1-"+_from).removeClass("active");
+        $(".m1-"+_to).addClass("active");
+        active = _to;
+
+    }
 };
 
 function changem2item(_from,_to)
 {
     if( _to == active2 ) return;
-    $("li.m2-"+_from).removeClass("item-active");
-    $("li.m2-"+_to).addClass("item-active");
-    $("div.m2-"+_from).fadeOut(300,function(){$("div.m2-"+_to).slideDown(600);});
-    active2 = _to;
+    if( !change )
+    {
+        change = true;
+        $("li.m2-"+_from).removeClass("item-active");
+        $("li.m2-"+_to).addClass("item-active");
+        $("div.m2-"+_from).fadeOut(300,function(){$("div.m2-"+_to).slideDown(600); change = false;});
+        active2 = _to;
+    }
 };
