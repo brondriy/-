4dc70c38e058a55e91072aa945d247de7003000c
brondriy <ahremchik.mani@gmail.com>
2017-02-03 12:06:05 +0300

    page2-jquery

--- js/js.js
+++ js/js.js
@@ -1,3 +1,6 @@
+var active = 1;
+var active2 = 1;
+
 $(document).ready(function(){
     $(".p1-center-text").animate(
     {
@@ -14,8 +17,34 @@ $(document).ready(function(){
     }, 5000);
 
 });
-$(".d1").click(function()
-    {
-        /*$(".main1").slideUp(600,function(){$(".main2").slideDown(600);});*/
-        $(".main1").fadeOut(600,function(){$(".main2").slideDown(600);});
-});
+
+
+$(".d1").click(function(){changepage(1,2);});
+
+/*header*/
+$(".m1-1").click(function(){changepage(active,1);});
+$(".m1-2").click(function(){changepage(active,2);});
+$(".logo,.logo-hover").click(function(){changepage(active,1);});
+
+/*page2*/
+$(".m2-1").click(function(){changem2item(active2,1);});
+$(".m2-2").click(function(){changem2item(active2,2);});
+
+
+function changepage(_from,_to)
+{
+    if( _to == active ) return;
+    $(".main"+_from).fadeOut(600,function(){$(".main"+_to).slideDown(600);});
+    $(".m1-"+_from).removeClass("active");
+    $(".m1-"+_to).addClass("active");
+    active = _to;
+};
+
+function changem2item(_from,_to)
+{
+    if( _to == active2 ) return;
+    $(".m2-"+_from).removeClass("item-active");
+    $(".m2-"+_to).addClass("item-active");
+    $(".vcenter").html("<p>"+$(".m2-"+_to).text()+"</p> <br> LABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABLABL");
+    active2 = _to;
+};
